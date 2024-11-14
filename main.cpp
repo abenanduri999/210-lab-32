814544c48e4d4fc6658938b8a3e6993a49dc9847
Abe <abenanduri1@gmail.com>
2024-11-13 20:10:20 -0800

    4th commit

--- main.cpp
+++ main.cpp
@@ -4,11 +4,19 @@
 
 using namespace std; 
 
+const int CARS = 2;
+
 int main() 
 {
     deque<Car> line; 
 
+    for(int i = 0; i < CARS; i++)
+    {
+        line.push_back(Car());
+    
+    }
 
+    
 
 
 
