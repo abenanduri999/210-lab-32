6ac5f97c359aedc27fa3a459da644ad37b02de0c
Abe <abenanduri1@gmail.com>
2024-11-13 22:39:44 -0800

    8th commit

--- main.cpp
+++ main.cpp
@@ -14,8 +14,10 @@ int main()
     for(int i = 0; i < CARS; i++)
     {
         line.push_back(Car());
+        cout<<"\t";
         line[i].print(); 
     }
+    cout<<"\n"; 
 
     srand(time(0)); 
     int i = 1; 
@@ -32,8 +34,10 @@ int main()
                 cout<<"Queue: "<<endl;
                 for(int k = 0; k < line.size(); k++ )
                 {
+                    cout<<"\t";
                     line[k].print();
                 }
+                cout<<endl;
             }
         else
             {
@@ -41,14 +45,24 @@ int main()
                 cout<<"Time: "<<i<<" Operation: Car Paid: ["<<firstElement.getYear()<<" "<<firstElement.getMake()
                 <<" ("<<firstElement.getTransponder()<<")]"<<endl;
                 line.pop_front(); 
-                    
+                cout<<"Queue: "<<endl;
+                for(int j = 0; j < line.size(); j++ )
+                {
+                    cout<<"\t";
+                    line[j].print();
+                }
+                cout<<endl;    
             }
         i++; 
 
     } while(!line.empty()); 
 
 
-
+    if(line.empty())
+    {
+        cout<<"Queue:\n";
+        cout<<"\tEmpty"<<endl;
+    }
 
 
 
