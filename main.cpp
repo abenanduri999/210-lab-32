4dee231cd49ab4697b9bb9b1d1714204205a7d04
Abe <abenanduri1@gmail.com>
2024-11-13 21:41:53 -0800

    5th commit

--- main.cpp
+++ main.cpp
@@ -10,13 +10,26 @@ int main()
 {
     deque<Car> line; 
 
+    cout<<"Initial Queue:"<<endl; 
     for(int i = 0; i < CARS; i++)
     {
         line.push_back(Car());
-    
+        line[i].print(); 
     }
 
-    
+    srand(time(0)); 
+    int i = 1; 
+    /*do{
+
+        int prob = rand() % 100 + 1; 
+
+        if(prob <= 55 ) 
+            {
+
+            }
+        
+
+    } while(!line.empty()); */
 
 
 
